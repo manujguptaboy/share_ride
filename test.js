20f443f636eaa1d8e6f2b8338225ae85edb4dbbd
manuj gupta <manujgupta@manujs-MacBook-Air.local>
2026-04-26 13:52:49 +0530

    test git

---
+++ test.js
@@ -0,0 +1,14 @@
+manujesh = {
+    name: "manujesh",
+    age: 20,
+    city: "delhi",
+    country: "india",
+    email: "manujesh@gmail.com",
+    phone: "1234567890",
+    address: "1234567890",
+    city: "delhi",
+    country: "india",
+    email: "manujesh@gmail.com",
+    phone: "1234567890",
+    address: "1234567890",
+}
